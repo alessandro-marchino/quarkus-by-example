0cda38b1d54b32cf9f70d55710298ce286e21872
amarchino <marchino.alessandro@gmail.com>
2026-06-07 17:56:29 +0200

    Lesson 4

---
+++ smartbar-backoffice/src/test/java/de/schulte/SmartbarBackofficeIT.java
@@ -0,0 +1,8 @@
+package de.schulte;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class SmartbarBackofficeIT extends SmartbarBackofficeTest {
+    // Execute the same tests but in packaged mode.
+}
