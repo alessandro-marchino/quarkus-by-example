c44366e38eb13ed7a7bedce2149f9637873ce5db
amarchino <marchino.alessandro@gmail.com>
2026-06-14 15:34:11 +0200

    Lesson 20

--- smartbar-backoffice/src/main/java/de/schulte/smartbar/backoffice/api/ArticlesResource.java
+++ smartbar-backoffice/src/main/java/de/schulte/smartbar/backoffice/api/ArticlesResource.java
@@ -4,10 +4,21 @@ import java.net.URI;
 import java.util.List;
 
 import de.schulte.smartbar.backoffice.api.model.Article;
+import de.schulte.smartbar.backoffice.service.ArticlesService;
+import io.smallrye.common.annotation.NonBlocking;
+import jakarta.inject.Inject;
 import jakarta.validation.Valid;
 import jakarta.ws.rs.core.Response;
 
+@NonBlocking
 public class ArticlesResource implements ArticlesApi {
+
+    private final ArticlesService articlesService;
+
+    @Inject
+    public ArticlesResource(ArticlesService articlesService) {
+        this.articlesService = articlesService;
+    }
 
     @Override
     public Response articlesArticleIdDelete(String articleId) {
@@ -16,7 +27,7 @@ public class ArticlesResource implements ArticlesApi {
 
     @Override
     public Response articlesArticleIdGet(String articleId) {
-        return Response.ok(new Article().name("Chardonnay")).build();
+        return Response.ok(articlesService.get()).build();
     }
 
     @Override
@@ -26,7 +37,7 @@ public class ArticlesResource implements ArticlesApi {
 
     @Override
     public Response articlesGet() {
-        return Response.ok(List.of(new Article().name("Chardonnay"))).build();
+        return Response.ok(List.of(articlesService.get())).build();
     }
 
     @Override
