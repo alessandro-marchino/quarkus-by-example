1be59139820888c0fcc0aa6c4f4f8a242187928e
amarchino <marchino.alessandro@gmail.com>
2026-07-25 22:50:07 +0200

    Lesson 52

--- smartbar-backoffice/src/main/java/de/schulte/smartbar/backoffice/tables/TablesResource.java
+++ smartbar-backoffice/src/main/java/de/schulte/smartbar/backoffice/tables/TablesResource.java
@@ -1,9 +1,11 @@
 package de.schulte.smartbar.backoffice.tables;
 
 import io.quarkus.hibernate.reactive.rest.data.panache.PanacheRepositoryResource;
+import io.quarkus.rest.data.panache.ResourceProperties;
 import io.smallrye.common.annotation.NonBlocking;
 
 @NonBlocking
+@ResourceProperties(rolesAllowed = { "admin" })
 public interface TablesResource extends PanacheRepositoryResource<TablesRepository, Table, Long> {
     // Empty?
 }
