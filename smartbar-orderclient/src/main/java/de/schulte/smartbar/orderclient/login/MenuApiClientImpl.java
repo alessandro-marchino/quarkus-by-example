e8f75302bd752cbf01c59ee66198b3bca6ad7c4c
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2026-08-04 12:31:22 +0200

    Lesson 68

--- smartbar-orderclient/src/main/java/de/schulte/smartbar/orderclient/login/MenuApiClientImpl.java
+++ smartbar-orderclient/src/main/java/de/schulte/smartbar/orderclient/login/MenuApiClientImpl.java
@@ -7,6 +7,10 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import com.fasterxml.jackson.databind.json.JsonMapper;
@@ -18,19 +22,19 @@ import jakarta.enterprise.context.ApplicationScoped;
 public class MenuApiClientImpl implements MenuApiClient {
 
 	private final String apiUrl;
+	private final JsonMapper jsonMapper;
 
 	public MenuApiClientImpl(@ConfigProperty(name = "backoffice.menuapi.url") String apiUrl) {
 		this.apiUrl = apiUrl;
+		this.jsonMapper = new JsonMapper();
 	}
 
 	@Override
 	public ApiMenu getMenu() {
-		try {
-			final var request = HttpRequest.newBuilder(new URI(apiUrl)).GET().build();
-			final var httpClient = HttpClient.newBuilder().build();
-			final var response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-			return new JsonMapper().readValue(response.body(), ApiMenu.class);
-		} catch (URISyntaxException | IOException | InterruptedException e) {
+		final var request = new HttpGet(apiUrl);
+		try(CloseableHttpClient client = HttpClients.createDefault()) {
+			return client.execute(request, response -> jsonMapper.readValue(response.getEntity().getContent(), ApiMenu.class));
+		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 	}
