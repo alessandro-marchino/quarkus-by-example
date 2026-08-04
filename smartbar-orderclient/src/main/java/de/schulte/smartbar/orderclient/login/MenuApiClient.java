5642472f78e23a9f1d5d15f360052cd919e90230
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2026-08-04 13:51:39 +0200

    Lesson 76

--- smartbar-orderclient/src/main/java/de/schulte/smartbar/orderclient/login/MenuApiClient.java
+++ smartbar-orderclient/src/main/java/de/schulte/smartbar/orderclient/login/MenuApiClient.java
@@ -1,5 +1,8 @@
 package de.schulte.smartbar.orderclient.login;
 
+import java.util.List;
+
+import org.eclipse.microprofile.faulttolerance.Fallback;
 import org.eclipse.microprofile.faulttolerance.Retry;
 import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
@@ -16,5 +19,12 @@ public interface MenuApiClient {
 	@GET
 	@Retry(maxRetries = 10)
 	@Timeout(50)
+	@Fallback(fallbackMethod = "getFallbackMenu")
 	Uni<ApiMenu> getMenu();
+
+	default Uni<ApiMenu> getFallbackMenu() {
+		final var apiMenu = new ApiMenu();
+		apiMenu.setCategories(List.of());
+		return Uni.createFrom().item(apiMenu);
+	}
 }
