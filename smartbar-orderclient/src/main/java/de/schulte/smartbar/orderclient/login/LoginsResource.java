005ede53c7bfc66805ea9d3f023527c4e9f67a95
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2026-08-04 16:14:50 +0200

    Lesson 84

--- smartbar-orderclient/src/main/java/de/schulte/smartbar/orderclient/login/LoginsResource.java
+++ smartbar-orderclient/src/main/java/de/schulte/smartbar/orderclient/login/LoginsResource.java
@@ -5,33 +5,28 @@ import org.eclipse.microprofile.rest.client.inject.RestClient;
 import de.schulte.smartbar.orderclient.api.LoginsApi;
 import de.schulte.smartbar.orderclient.api.model.LoginResponseBody;
 import io.smallrye.mutiny.Uni;
-import io.quarkus.cache.CacheInvalidate;
-import io.quarkus.cache.CacheResult;
+import io.quarkus.cache.Cache;
+import io.quarkus.cache.CacheName;
 import io.smallrye.common.annotation.NonBlocking;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.DELETE;
 
 @NonBlocking
 public class LoginsResource implements LoginsApi {
 
 	private final MenuApiClient menuApiClient;
 	private final MenuMapper menuMapper;
+	private final Cache cache;
 
 	@Inject
-	public LoginsResource(@RestClient MenuApiClient menuApiClient, MenuMapper menuMapper) {
+	public LoginsResource(@RestClient MenuApiClient menuApiClient, MenuMapper menuMapper, @CacheName("menu-cache") Cache cache) {
 		this.menuApiClient = menuApiClient;
 		this.menuMapper = menuMapper;
+		this.cache = cache;
 	}
 
 	@Override
-	@CacheResult(cacheName = "menu-cache")
 	public Uni<LoginResponseBody> login(String tableId) {
-		return menuApiClient.getMenu().map(menuMapper::mapToLoginResponse);
+		return cache.getAsync(tableId, _ -> menuApiClient.getMenu().map(menuMapper::mapToLoginResponse));
 	}
 
-	// @CacheInvalidate(cacheName = "menu-cache")
-	// @DELETE
-	// public Uni<String> invalidateCacheByKey(String tableId) {
-	// 	return Uni.createFrom().item("Ok");
-	// }
 }
