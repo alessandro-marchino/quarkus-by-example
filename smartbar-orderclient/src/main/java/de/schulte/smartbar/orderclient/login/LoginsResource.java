7650fdf1ab9c974c12ae02307bb21ca9c9859d2e
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2026-08-04 12:51:57 +0200

    Lesson 70

--- smartbar-orderclient/src/main/java/de/schulte/smartbar/orderclient/login/LoginsResource.java
+++ smartbar-orderclient/src/main/java/de/schulte/smartbar/orderclient/login/LoginsResource.java
@@ -1,5 +1,7 @@
 package de.schulte.smartbar.orderclient.login;
 
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
 import de.schulte.smartbar.orderclient.api.LoginsApi;
 import de.schulte.smartbar.orderclient.api.model.LoginResponseBody;
 // import io.smallrye.common.annotation.NonBlocking;
@@ -12,7 +14,7 @@ public class LoginsResource implements LoginsApi {
 	private final MenuMapper menuMapper;
 
 	@Inject
-	public LoginsResource(MenuApiClient menuApiClient, MenuMapper menuMapper) {
+	public LoginsResource(@RestClient MenuApiClient menuApiClient, MenuMapper menuMapper) {
 		this.menuApiClient = menuApiClient;
 		this.menuMapper = menuMapper;
 	}
--- smartbar-orderclient/src/main/java/de/schulte/smartbar/orderclient/login/MenuApiClient.java
+++ smartbar-orderclient/src/main/java/de/schulte/smartbar/orderclient/login/MenuApiClient.java
@@ -1,8 +1,15 @@
 package de.schulte.smartbar.orderclient.login;
 
-import de.schulte.smartbar.backoffice.api.model.ApiMenu;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
+import de.schulte.smartbar.backoffice.api.model.ApiMenu;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+@Path("/menu")
+@RegisterRestClient
 public interface MenuApiClient {
 
+	@GET
 	ApiMenu getMenu();
 }
