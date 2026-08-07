ca1dadc3c3ee99580b4ffdf2005de7b6c5b90f37
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2026-08-07 12:51:02 +0200

    Lesson 90

--- smartbar-orderclient/src/main/java/de/schulte/smartbar/orderclient/login/RedisLoginService.java
+++ smartbar-orderclient/src/main/java/de/schulte/smartbar/orderclient/login/RedisLoginService.java
@@ -1,40 +1,35 @@
 package de.schulte.smartbar.orderclient.login;
 
+import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
-import org.jspecify.annotations.NonNull;
-
-import io.quarkus.redis.client.RedisClientName;
-import io.quarkus.redis.datasource.ReactiveRedisDataSource;
-import io.quarkus.redis.datasource.keys.ReactiveKeyCommands;
-import io.quarkus.redis.datasource.value.ReactiveValueCommands;
 import io.smallrye.mutiny.Uni;
+import io.vertx.mutiny.redis.client.RedisAPI;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
 @ApplicationScoped
 public class RedisLoginService implements LoginService {
 
-	private ReactiveValueCommands<String, @NonNull String> tokens;
-	private ReactiveKeyCommands<@NonNull String> keys;
+	private final RedisAPI redisAPI;
 
 	@Inject
-	public RedisLoginService(@RedisClientName("logins") ReactiveRedisDataSource reactiveRedisDataSource) {
-		tokens = reactiveRedisDataSource.value(String.class);
-		keys = reactiveRedisDataSource.key(String.class);
+	public RedisLoginService(RedisAPI redisAPI) {
+		this.redisAPI = redisAPI;
 	}
 
 	@Override
 	public Uni<String> createNewLogin(String tableId) {
 		String token = UUID.randomUUID().toString();
-		return tokens.set(tableId, token)
-			.chain(v -> keys.expire(tableId, 20))
-			.map(v -> token);
+		return redisAPI.set(List.of(tableId, token, "EX", "20"))
+			.map(r -> token);
 	}
 
 	@Override
 	public Uni<Boolean> hasLogin(String tableId) {
-		return keys.exists(tableId);
+		return redisAPI.get(tableId)
+			.map(Objects::nonNull);
 	}
 
 }
