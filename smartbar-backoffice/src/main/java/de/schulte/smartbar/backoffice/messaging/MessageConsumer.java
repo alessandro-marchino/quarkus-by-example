9e77f34d718d718625db07210586241098388a3a
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2026-08-25 13:55:12 +0200

    Lesson 102

--- smartbar-backoffice/src/main/java/de/schulte/smartbar/backoffice/messaging/MessageConsumer.java
+++ smartbar-backoffice/src/main/java/de/schulte/smartbar/backoffice/messaging/MessageConsumer.java
@@ -4,7 +4,6 @@ import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 
 import io.quarkus.logging.Log;
-import io.smallrye.reactive.messaging.annotations.Broadcast;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
@@ -12,13 +11,13 @@ public class MessageConsumer {
 
 	@Incoming("my-channel")
 	@Outgoing("uppercase-channel")
-	@Broadcast
 	public String consume(String message) {
 		Log.infof("Message %s consumed in %s", message, getClass().getSimpleName());
 		return message.toUpperCase();
 	}
 
 	@Incoming("my-channel")
+	@Outgoing("uppercase-channel")
 	public String consume2(String message) {
 		Log.infof("2 - Message %s consumed in %s", message, getClass().getSimpleName());
 		return message.toUpperCase();
--- smartbar-backoffice/src/main/java/de/schulte/smartbar/backoffice/messaging/UppercaseConsumer.java
+++ smartbar-backoffice/src/main/java/de/schulte/smartbar/backoffice/messaging/UppercaseConsumer.java
@@ -3,18 +3,16 @@ package de.schulte.smartbar.backoffice.messaging;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 
 import io.quarkus.logging.Log;
+import io.smallrye.reactive.messaging.annotations.Merge;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class UppercaseConsumer {
 
 	@Incoming("uppercase-channel")
+	@Merge
 	public void consume(String message) {
 		Log.infof("Message %s consumed in %s", message, getClass().getSimpleName());
 	}
 
-	@Incoming("uppercase-channel")
-	public void consume2(String message) {
-		Log.infof("2 - Message %s consumed in %s", message, getClass().getSimpleName());
-	}
 }
